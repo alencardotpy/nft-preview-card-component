a5b406cfd6b53b633df56192274fcb8b2a2e4e20
Carlos Alencar <68407281+alencardotpy@users.noreply.github.com>
2022-07-03 14:23:55 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import Card from "./Components/Card/Card";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
 
 const container = document.getElementById("root");
-root = createRoot(container);
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
